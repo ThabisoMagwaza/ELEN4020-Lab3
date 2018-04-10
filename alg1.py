7bbf698cdf102b07c77edadb624cfd62667c38f8
836403@students.wits.ac.za <836403@students.wits.ac.za>
2018-04-11 00:39:12 +0200

    Initial commit

---
+++ alg1.py
@@ -0,0 +1,53 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import os
+import re
+import sys
+import time
+
+
+class onestepMatrixMult(MRJob):
+	def steps(self):
+		return [MRStep(mapper = self.mapper,reducer = self.first_reducer),MRStep(reducer = self.second_reducer)]
+	
+	def mapper(self,_,value):
+	
+		if(len(value.split())) < 3:
+			return
+			
+		i,j,val = value.split()
+		matrixName = os.environ['map_input_file']
+		
+		#if matrixName == "Matrix1.txt":
+		if matrixName == "Matrix1.txt":
+			yield j, ('M',i,val)
+		else:
+			# j,k,val = i,j,val
+			# yield(j,('N',k,val)
+			yield i,('N',j,val)
+		
+	
+	def first_reducer(self,key,values):
+		listM = [];
+		listN = [];
+
+		for value in values:
+			if value[0] == 'M':
+				listM.append((value[1],value[2]))
+			else:
+				listN.append((value[1],value[2]))
+		
+		for elem1 in listM:
+			for elem2 in listN:
+				yield (int(elem1[0]),int(elem2[0])),(int(elem1[1])*int(elem2[1]))
+			
+		print("reducer1 done")
+	
+	def second_reducer(self,key,values):
+		yield key,sum(values)
+	print("reducer done")
+		
+if __name__ == '__main__':
+	start_time = time.time()
+	onestepMatrixMult.run()
+	print("--- %s seconds ---" % (time.time() - start_time))
---
+++ alg2.py
@@ -0,0 +1,58 @@
+from mrjob.job import MRJob
+import re
+import os
+import sys
+import time
+
+
+class onesStepMatrixMult(MRJob):
+	
+	def mapper(self,_,value):
+		if len(value.split()) == 2:
+			return
+			
+		row,col,val = value.split()
+		matrixName = os.environ['map_input_file']
+		
+		if matrixName == "Matrix1.txt":
+			for c in range(0,k):
+				yield (int(row),c),('M',int(col),int(val))
+		else:
+			for r in range(0,i):
+				# j,k,val = i,j,val
+				yield (r,int(col)),('N',int(row),int(val))
+	
+	def reducer(self,key,values):
+		listM = [0] * j
+		listN = [0] * j
+		listTupple = []
+		
+		for value in values:
+			if value[0] == 'M':
+				listM[value[1]] = value[2]
+			else:
+				listN[value[1]] = value[2]
+		
+		for it in range(0,j):
+			listTupple.append(listM[it]*listN[it])
+		yield key , sum(listTupple)
+		print("reducer done")
+
+if __name__ == '__main__':
+	
+	matrix1 = open('Matrix1.txt','r')
+	matrix2 = open('Matrix2.txt','r')
+	
+	i,j_1 = matrix1.readline().split()
+	j_2,k = matrix2.readline().split()
+	
+	i = int(i)
+	k = int(k)
+	j = int(j_1)
+
+	
+	matrix1.close()
+	matrix2.close()
+	start_time = time.time()
+	onesStepMatrixMult.run()
+	print("--- %s seconds ---" % (time.time() - start_time))
